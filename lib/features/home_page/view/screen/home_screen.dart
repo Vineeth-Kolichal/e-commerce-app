4fb88e7fd0211024c838aaa0f43fb993ccfc10c3
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2023-11-30 00:01:13 +0530

    home UI skelton almost completed

--- lib/features/home_page/view/screen/home_screen.dart
+++ lib/features/home_page/view/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_machine_test_jurysoft/utils/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -51,7 +52,7 @@ class HomeScreen extends StatelessWidget {
               crossAxisCount: 2,
               mainAxisSpacing: 7,
               crossAxisSpacing: 7,
-              childAspectRatio: 0.65,
+              childAspectRatio: 0.63,
             ),
           ),
         ],
@@ -67,58 +68,81 @@ class ProductGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AppTheme.whiteColor,
-        border: Border.all(width: 0.5, color: Colors.black),
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(255, 224, 224, 224),
-            blurRadius: 3,
-            spreadRadius: 0.5,
-            offset: Offset(0.0, 4.0), // shadow direction: bottom right
-          ),
-        ],
-      ),
-      alignment: Alignment.center,
-      //color: ,
-      child: Column(
-        children: [
-          Expanded(
-              child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              'https://rukminim1.flixcart.com/image/850/1000/xif0q/mobile/1/d/y/-original-imaghxcpvtta2hzs.jpeg?q=90',
-              fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppTheme.whiteColor,
+          border: Border.all(width: 0.5, color: Colors.black),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 224, 224, 224),
+              blurRadius: 3,
+              spreadRadius: 0.5,
+              offset: Offset(0.0, 4.0), // shadow direction: bottom right
             ),
-          )),
-          LayoutBuilder(builder: (context, constraints) {
-            print("${constraints.maxWidth}");
-            return Container(
-              padding: EdgeInsets.all(6),
-              width: constraints.maxWidth,
-              height: 80,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Iphone",
-                    style: txt15BlackSB,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    'Apple I phone 14 (Blue, 124)',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: txt12GreySB,
-                  )
-                ],
+          ],
+        ),
+        alignment: Alignment.center,
+        //color: ,
+        child: Column(
+          children: [
+            Expanded(
+                child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                'https://rukminim1.flixcart.com/image/850/1000/xif0q/mobile/1/d/y/-original-imaghxcpvtta2hzs.jpeg?q=90',
+                fit: BoxFit.cover,
               ),
-            );
-          }),
-        ],
+            )),
+            LayoutBuilder(builder: (context, constraints) {
+              print("${constraints.maxWidth}");
+              return Container(
+                padding: EdgeInsets.all(6),
+                width: constraints.maxWidth,
+                height: 90,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Iphone",
+                      style: txt15BlackSB,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      'Apple I phone 14 (Blue, 124)',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: txt12GreySB,
+                    ),
+                    Text(
+                      "₹670000",
+                      style: txt17BlackB,
+                    ),
+                    RatingBar.builder(
+                      itemSize: 15,
+                      initialRating: 2.3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        color: Colors.green,
+                      ),
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -9,5 +9,7 @@ class AppTheme {
 
 const txt15BlackSB = TextStyle(
     fontSize: 15, fontWeight: FontWeight.w600, color: AppTheme.blackColor);
+const txt17BlackB = TextStyle(
+    fontSize: 17, fontWeight: FontWeight.w700, color: AppTheme.blackColor);
 const txt12GreySB = TextStyle(
     fontSize: 12, fontWeight: FontWeight.w600, color: AppTheme.greyColor);
